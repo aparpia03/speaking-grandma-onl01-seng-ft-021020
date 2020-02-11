994414ad62311ac31671fcc7944dd99051e73e62
Abbas Parpia <aparpia03@gmail.com>
2020-02-11 18:07:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,18 +17,11 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  else if phrase == phrase.downcase
-    return "HUH?! SPEAK UP, SONNY!"
-  else if phrase == phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  else if phrase == phrase.downcase
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
-
-
-
 
 
