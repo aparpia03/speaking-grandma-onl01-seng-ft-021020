1050aca4a7b5089d26ef72810c9d5736b02c73f6
Abbas Parpia <aparpia03@gmail.com>
2020-02-11 17:15:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,6 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  else if phrase == 
+  else if phrase == phrase.downcase
+    puts "HUH! SPEAK UP, 
 end
