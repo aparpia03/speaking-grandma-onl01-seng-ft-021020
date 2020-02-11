e00617190eed42f251061f90376897de607d810c
Abbas Parpia <aparpia03@gmail.com>
2020-02-11 17:30:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,14 +13,6 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  else if phrase == phrase.downcase
-    puts "HUH! SPEAK UP, SONNY!"
-  else if phrase === phrase.upcase
-    puts "HUH! SPEAK UP, SONNY!"
-  else if phrase === phrase.downcase
-    puts "NO, NOT SINCE 1938!"
-  else phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+  if phrase.upcase
+    
 end
